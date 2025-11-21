d011062a91504efe1d080752c0e37efd06d85217
Habib Oyewo <habiboyewo98@gmail.com>
2025-11-20 16:01:48 -0800

    typeing error fixed

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,16 +1,26 @@
-// IdPageServer.tsx
 import prisma from "@/app/utils/db";
 import PostDetailClient from "@/components/general/PostDetailsClient";
 import { notFound } from "next/navigation";
 
-
 async function getData(id: string) {
-  const data = await prisma.blogPost.findUnique({ where: { id } });
-  if (!data) return notFound();
+  const data = await prisma.blogPost.findUnique({
+    where: { id },
+  });
+
+  if (!data) {
+    return notFound();
+  }
+
   return data;
 }
 
-export default async function IdPage({ params }: { params: { id: string } }) {
-  const data = await getData(params.id);
-  return <PostDetailClient post={data}/>;
+export default async function IdPage({
+  params,
+}: {
+  params: Promise<{ id: string }>; 
+}) {
+  const { id } = await params; 
+  const data = await getData(id);
+
+  return <PostDetailClient post={data} />;
 }
